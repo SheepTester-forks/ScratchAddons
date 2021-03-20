004e887fa46f0db2511f732d6936ec08c4a61c3a
SheepTester <ussa.say@gmail.com>
2021-03-19 20:46:22 -0700

    Prevent opening the context menu when holding down the left mouse button

--- addons/click-hold-context-menu-fix/userscript.js
+++ addons/click-hold-context-menu-fix/userscript.js
@@ -1,48 +1,59 @@
-export default function ({ addon, global, console }) {
-  const ALLOWED_MENU = 'sa-allowed-context-menu';
+export default async function ({ addon, global, console }) {
+  window.addon = addon;
 
   let leftMouseDown = false;
   let rightMouseDown = false;
   // Touch and pen trigger mousedown and mouseup in quick succession, so
   // `leftMouseDown` won't be set to true long enough for them.
-  window.addEventListener('mousedown', (e) => {
+  document.addEventListener('mousedown', (e) => {
     if (e.button === 0) {
       leftMouseDown = true;
     } else if (e.button === 2) {
       rightMouseDown = true;
     }
   });
-  window.addEventListener('mouseup', (e) => {
+  document.addEventListener('mouseup', (e) => {
     if (e.button === 0) {
       leftMouseDown = false;
     } else if (e.button === 2) {
       rightMouseDown = false;
     }
   });
+  // click is fired after mouseup
+  document.addEventListener('click', (e) => {
+    if (e.button === 0 && !rightMouseDown) {
+      window.requestAnimationFrame(() => {
+        leftMouseDown = true
+        window.requestAnimationFrame(() => {
+          // In the second animation frame after the click, this is when Scratch
+          // unhides the context menu for some reason:
+          // 0. click event handler
+          //    [animation frame]
+          // 1. first animation frame callback
+          //    [animation frame]
+          // 2. Scratch unhides the context menu
+          //    second animation frame callback
+          leftMouseDown = false
+        })
+      })
+    }
+  });
 
-  window.addon = addon;
-  (async () => {
-    // Check for open menus and close them if the user's left mouse button is
-    // held down.
-    while (true) {
-      const menu = await addon.tab.waitForElement(`nav[class*="context-menu_context-menu"][style*="opacity: 1;"]:not(.${ALLOWED_MENU})`);
-      console.log(menu, leftMouseDown, rightMouseDown)
-      // Right clicking whilst left clicking should still open the context menu
-      if (leftMouseDown && !rightMouseDown) {
-        menu.style.opacity = 0;
-        menu.style.pointerEvents = "none";
-      } else {
-        menu.classList.add(ALLOWED_MENU);
+  while (true) {
+    const menu = await addon.tab.waitForElement(`nav[class*="context-menu_context-menu"]`, { markAsSeen: true });
+    const observer = new MutationObserver(() => {
+      if (menu.style.opacity === "1") {
+        // Right clicking whilst left clicking should still open the context
+        // menu
+        if (leftMouseDown && !rightMouseDown) {
+          menu.style.opacity = 0;
+          menu.style.pointerEvents = "none";
+        }
       }
-    }
-  })();
-
-  (async () => {
-    // Unmark closed menus that had been allowed to be open so that they can be
-    // re-detected in the future.
-    while (true) {
-      const menu = await addon.tab.waitForElement(`nav[class*="context-menu_context-menu"][style*="opacity: 0;"].${ALLOWED_MENU}`);
-      menu.classList.remove(ALLOWED_MENU);
-    }
-  })();
+    });
+    observer.observe(menu, {
+      attributes: true,
+      attributeFilter: ['style']
+    });
+  }
 }
