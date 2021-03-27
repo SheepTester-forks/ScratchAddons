6b2eb655fa8d53b8b4f094302c23079a7dde5e7c
SheepTester <ussa.say@gmail.com>
2021-03-27 15:58:15 -0700

    Add "Upload HD costume" button that inserts a PNG inside a vector costume

---
+++ addons/high-res-png/to-svg.js
@@ -0,0 +1,62 @@
+function svgify({ url, width, height, scale = 1 }) {
+  return new Blob(
+    [
+      `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="${
+        width * scale
+      }" height="${height * scale}" viewBox="0 0 ${width * scale} ${
+        height * scale
+      }"><image x="0" y="0" width="${width}" height="${height}" transform="scale(${scale}, ${scale})" xlink:href="${url}" /></svg>`,
+    ],
+    { type: "image/svg+xml" }
+  );
+}
+
+function dataUrlify(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      resolve(reader.result);
+    });
+    reader.addEventListener("error", () => {
+      reject(reader.error);
+    });
+    reader.readAsDataURL(blob);
+  });
+}
+
+function getImageSize(blob) {
+  return new Promise((resolve, reject) => {
+    const image = document.createElement("img");
+    const url = URL.createObjectURL(blob);
+    image.addEventListener("load", () => {
+      resolve({ width: image.width, height: image.height });
+      URL.revokeObjectURL(url);
+    });
+    image.addEventListener("error", () => {
+      reject(new TypeError("Image failed to load. (This might mean the image is invalid.)"));
+    });
+    image.src = url;
+  });
+}
+
+// TODO: Do we support custom stage sizes?
+const MAX_WIDTH = 480;
+const MAX_HEIGHT = 360;
+
+export async function fileToSvg(file) {
+  const { width, height } = await getImageSize(file);
+  const url = await dataUrlify(file);
+  let scale = 1;
+  if (width > MAX_WIDTH) {
+    scale = MAX_WIDTH / width;
+  }
+  if (scale * height > MAX_HEIGHT) {
+    scale *= MAX_HEIGHT / height;
+  }
+  return svgify({
+    url,
+    width: width * scale,
+    height: height * scale,
+    scale,
+  });
+}
---
+++ addons/high-res-png/userscript.js
@@ -0,0 +1,80 @@
+import { fileToSvg } from "./to-svg.js";
+
+export default async function ({ addon, global, console, msg }) {
+  const vm = addon.tab.traps.vm;
+
+  async function addSvgCostume(svg, name) {
+    const storage = vm.runtime.storage;
+    const targetId = vm.editingTarget.id;
+    const asset = storage.createAsset(
+      storage.AssetType.ImageVector,
+      storage.DataFormat.SVG,
+      new Uint8Array(await svg.arrayBuffer()),
+      null,
+      true // generate md5
+    );
+    const costume = {
+      name,
+      dataFormat: storage.DataFormat.SVG,
+      asset,
+      md5: `${asset.assetId}.${storage.DataFormat.SVG}`,
+      assetId: asset.assetId,
+    };
+    await vm.addCostume(costume.md5, costume, targetId);
+  }
+
+  function createUploadBtn() {
+    const icon = Object.assign(document.createElement("img"), {
+      className: addon.tab.scratchClass("action-menu_more-icon"),
+      draggable: false,
+      src: "/static/assets/e587d9d7e9a2f1f28041ba5c15db7a40.svg",
+    });
+    const fileInput = Object.assign(document.createElement("input"), {
+      className: addon.tab.scratchClass("action-menu_file-input"),
+      accept: ".svg, .png, .bmp, .jpg, .jpeg, .gif",
+      multiple: true,
+      type: "file",
+    });
+    fileInput.addEventListener("change", async () => {
+      // Clone file list in case user changes it while loading
+      const files = [...fileInput.files];
+      for (const file of files) {
+        await addSvgCostume(await fileToSvg(file), file.name.split(".", 1)[0]);
+      }
+    });
+    const button = Object.assign(document.createElement("button"), {
+      className: addon.tab.scratchClass("action-menu_button", "action-menu_more-button"),
+      ariaLabel: msg("upload-hd-costume"),
+    });
+    button.addEventListener("click", () => {
+      // Scratch also blurs focus on click for action menu items
+      button.blur();
+      fileInput.click();
+    });
+    button.addEventListener("mouseenter", () => {
+      toolTip.classList.add("show");
+      const { top, height, right } = button.getBoundingClientRect();
+      const { height: toolTipHeight } = toolTip.getBoundingClientRect();
+      toolTip.style.left = right + "px";
+      toolTip.style.top = top + (height - toolTipHeight) / 2 + "px";
+    });
+    button.addEventListener("mouseleave", () => {
+      toolTip.classList.remove("show");
+    });
+    button.append(icon, fileInput);
+    const toolTip = Object.assign(document.createElement("div"), {
+      className: `__react_component_tooltip place-right type-dark ${addon.tab.scratchClass("action-menu_tooltip")}`,
+      textContent: msg("upload-hd-costume"),
+    });
+    const div = document.createElement("div");
+    div.append(button, toolTip);
+    return div;
+  }
+
+  while (true) {
+    const addCostumeButtons = await addon.tab.waitForElement('#react-tabs-3 div[class*="action-menu_more-buttons_"]', {
+      markAsSeen: true,
+    });
+    addCostumeButtons.insertBefore(createUploadBtn(), addCostumeButtons.children[1]);
+  }
+}
